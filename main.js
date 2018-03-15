81c36c98be6083f3f6f00e107e8a177b7cd0f31d
Dani <33174848+danielchc@users.noreply.github.com>
2018-03-15 20:37:00 +0100

    Algo más decente

--- main.js
+++ main.js
@@ -1,60 +1,43 @@
-function shuffle(array) {
-	var currentIndex = array.length, temporaryValue, randomIndex;
-	while (0 !== currentIndex) {
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	}
-
-	return array;
-}
-function colorize(){
-	if ($(window).width()<=1080){return;}
-	if ($(window).scrollTop()>=200){
-		$("header").css("background-color","#34233e");
-	}else{
-		$("header").css("background-color","transparent");
-	}
-}
 $(document).ready(function(){
-	colorize();
-	$(window).scroll(function(){
-		colorize();
+	$(".socialicon").on("mouseenter mouseleave",function(){
+		var src=$(this).attr("altsrc");
+		$(this).attr("altsrc",$(this).attr("src"));
+		$(this).attr("src",src);
 	});
-	
-	$( "img[altsrc]" ).hover(
-	  function() {
-		$( this ).attr("defaultsrc",$( this ).attr("src"));
-		$( this ).attr("src",$( this ).attr("altsrc"));
-		
-	  }, function() {
-		$( this ).attr("src",$( this ).attr("defaultsrc"));
-	  }
-	);
+	function shuffle(array) {
+		var currentIndex = array.length, temporaryValue, randomIndex;
+		while (0 !== currentIndex) {
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
+
+		return array;
+	}
 	writing=false; //Estase escribindo ou borrando
 	index=0; //Palabra actual
 	currlett=0; //Letra actual
 	paused=true;
 	setTimeout(function(){
-		paused=false; //Establece unha pausa antes de empezar de 2 segundos,para poder leer o texto
+		paused=false;
 	},2000);
-	usitios=["SysAdmin","Python","DNS","DHCP","PHP","VB.Net","SQL","JQuery","Javascript","HTML5","CSS3","Libgdx2d","Linux","Windows"];
-	sitios=shuffle(usitios); //Mezcla os datos
+	codes=["Python","PHP","VB.Net","SQL","JQuery","Javascript","HTML5","CSS3"];
+	codes=shuffle(codes);
 	setInterval(function(){
 		if (paused) return; //Se está pausado non segue
-		str=$("#ame").text() //Obten o texto
+		str=$("#codes").text() //Obten o texto
 		if(!writing){ //Borranse as letras
 			str=str.substring(0,str.length - 1);
 			if(str.length==0){ //Se non hai máis letras pasase a escribir
 				writing=true;
 			}
 		}else{
-			if (index==sitios.length){index=0;} //Si o index
+			if (index==codes.length){index=0;} //Si o index
 			
-			str+=sitios[index][currlett]; //Engadese a letra currlett da palabra sitios[index]
-			if (str.length==sitios[index].length){
+			str+=codes[index][currlett]; //Engadese a letra currlett da palabra codes[index]
+			if (str.length==codes[index].length){
 				index++; //Cambia de palabra
 				currlett=0;
 				writing=false;
@@ -66,19 +49,10 @@ $(document).ready(function(){
 				currlett++; //Cambiase de letra
 			}
 		}
-		$("#ame").text(str)
-	}, 200); //Establece que se cambie unha letra cada 200ms
-	
-	
-	$("#shownav").on("click",function(e){
-		event.stopPropagation();
-		if($("nav").css("display")=="block"){
-			$("nav").css("display","none");
-		}else{
-			$("nav").css("display","block");
-		}
+		$("#codes").text(str)
+	}, 200);
+	$("#arrowtoogle").on("click",function(){
+		$("#details").slideToggle();
+		$(this).toggleClass("fliped",400);
 	});
-	
-	
-	
 });
